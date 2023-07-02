d0bca06c3861f0d93ae960b566bf6ba24351a57e
Justin Caringal <jacfriday10th@gmail.com>
2023-07-02 18:38:06 -0500

    Working(?) version of ThreadPoolExecutor

--- list_playlist_example.py
+++ list_playlist_example.py
@@ -8,8 +8,9 @@ from time import time
 # Youtube Library
 import pytube
 
-# Multithreading Library
+# Multithreading Libraries
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import multiprocessing
 
 url = 'https://www.youtube.com/playlist?list=PLvaO_paR56p-SNDvQNboq2BXniEfxj8gQ'
 
@@ -27,9 +28,15 @@ start = time()
 print(f'Playlist: {playlist.title}')
 
 processes = []
-with ThreadPoolExecutor(max_workers = 10) as executor:
+errors = []
+with ThreadPoolExecutor(max_workers = multiprocessing.cpu_count()) as executor:
     for video in playlist.videos:
-        processes.append(executor.submit(extract_info, video))
+        try:
+            processes.append(executor.submit(extract_info, video))
+            print(f'Title: {video.title} - Author: {video.author}')
+        except:
+            print(f'-----Error with {video.url}-----')
+            errors.append(video)
 
 videos_info = []
 for task in as_completed(processes):
@@ -37,4 +44,8 @@ for task in as_completed(processes):
     print(task.result())
 
 elapsed = time() - start
-print(f'Elapsed time {elapsed : .2f}')
+print(f'Elapsed time {elapsed : .2f} secs')
+print(f'No. of Vidoes: {len(videos_info)}')
+    
+for error in errors:
+    print(error)
