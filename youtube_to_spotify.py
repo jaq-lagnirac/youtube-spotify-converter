ba16b112d46cb1a9775158fe6a067fae3d62026e
Justin Caringal <jacfriday10th@gmail.com>
2023-07-02 18:50:12 -0500

    Added elapsed time, ran test
    -About 547 secs to execute

--- youtube_to_spotify.py
+++ youtube_to_spotify.py
@@ -8,6 +8,8 @@ import sys
 import argparse
 import logging
 
+from time import time
+
 # Youtube Library
 import pytube
 
@@ -15,6 +17,11 @@ url = 'https://www.youtube.com/playlist?list=PLvaO_paR56p-SNDvQNboq2BXniEfxj8gQ'
 
 playlist = pytube.Playlist(url)
 
+start = time()
+
 print(f'Playlist - {playlist.title}')
 for video in playlist.videos:
     print(f'Video - {video.title} - {video.author}')
+
+elapsed = time() - start
+print(f'Elapsed: {elapsed : .2f} secs')
