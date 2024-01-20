b5470642b46b324564de74c3eee6c474832d9aa4
Justin Caringal <jacfriday10th@gmail.com>
2024-01-19 23:08:27 -0600

    Added argparse and URL/JSON handling

--- access_spotify.py
+++ access_spotify.py
@@ -5,10 +5,22 @@
 # connected github --> https://github.com/katiagilligan888/Spotify-Discover-Weekly/blob/main/discoverWeekly.py
 
 import os
+import json
 from time import time
 import spotipy # streamlines access to Spotify API
 from spotipy.oauth2 import SpotifyOAuth # authenticates permissions
-from flask import Flask, request, url_for, session, redirect # accesses HTTP requests
+
+from colorful_errors import error_exit, red, green, cyan
+from extract_youtube_playlist import get_playlist, process_playlist
+
+# sets up command line arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--url', help='YouTube playlist URL')
+parser.add_argument('-j', '--json', help='Relative JSON path')
+args = parser.parse_args()
+URL = args.url
+JSON = args.json
 
 # loads in private .env variables
 from dotenv import load_dotenv
@@ -17,12 +29,20 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 FLASK_KEY = os.getenv('FLASK_KEY')
 
-
+# configures Flask app
+from flask import Flask, request, url_for, session, redirect # accesses HTTP requests
 app = Flask(__name__) # initializes Flask app
 app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie' # set name of session cookie
 app.secret_key = FLASK_KEY # sets random key to sign cookie
-
 TOKEN_INFO = 'token_info' # sets key for token info in session dict
+
+VALID_PLAYLIST_URL = 'youtube.com/playlist?'
+REQUIRED_KEYS = {'playlist_title', 'videos_info', 'errors'}
+
+
+###
+# FLASK APP RUNNING
+###
 
 
 # route to handle logging in
@@ -63,8 +83,48 @@ def convert_youtube_to_spotify():
     # create a Spotipy instance with the access token
     sp = spotipy.Spotify(auth=token_info['access_token'])
 
+    # extracts dict based on user input
+    playlist_dict = None
+    if URL:
+        # checks URL to see if valid
+        if VALID_PLAYLIST_URL not in URL:
+            print(red('URL not valid.'))
+            return 'URL not valid.'
+        
+        # extracts playlist into dict
+        print(cyan('Extracting from URL.'))
+        playlist_dict = url_extraction(URL)
+
+    elif JSON:
+        # checks JSON path to see if valid
+        _, extension = os.path.splitext(JSON)
+        if extension.lower() != '.json':
+            print(red('JSON path not valid.'))
+            return 'JSON path not valid.'
+
+        # extracts playlist into dict
+        print(cyan('Extracting from JSON.'))
+        playlist_dict = json_extraction(JSON)
+
+    else:
+        print(red('URL or JSON path not included.'))
+        return 'URL or JSON path not included.'
+    
+
+    return 'Playlist successfully converted.'
+
+
 # function to get the token info from the session
 def get_token():
+    """getter for token info
+    
+    A function which gets the token info from the session
+    
+    Returns:
+        The token info to connect Flask and the Spotify API
+    
+    """
+    
     token_info = session.get(TOKEN_INFO, None)
     if not token_info:
         # if the token info is not found, redirect the user to the login route
@@ -82,6 +142,13 @@ def get_token():
 
 
 def create_spotify_oauth():
+    """authenticates Spotify API
+    
+    Returns:
+        SpotifyOAuth: A successful OAuth
+        
+    """
+    
     return SpotifyOAuth(
         client_id = CLIENT_ID,
         client_secret = CLIENT_SECRET,
@@ -90,4 +157,61 @@ def create_spotify_oauth():
     )
 
 
-app.run(debug=True)
+def url_extraction(url):
+    """Conducts URL extraction
+    
+    A function which performs full playlist URL extraction
+    to use later
+
+    Args:
+        url (str): A URL to a YouTube playlist
+
+    Returns:
+        dict: A dictionary of a playlist and its videos
+
+    
+    """
+
+    # gets playlist from URL
+    playlist = get_playlist(url)
+
+    # generates dictionary from playlist object
+    playlist_dict = process_playlist(playlist)
+
+    return playlist_dict
+
+def json_extraction(json_path):
+    """Conducts JSON extraction
+
+    A function which uses a previously generated playlist JSON
+    to use later
+
+    Args:
+        json_path (str): A relative path to a JSON file
+
+    Returns:
+        dict: A dictionary of a playlist and its videos
+
+    """
+
+    # checks existence of file
+    if not os.path.exists(json_path):
+        print(red(f'{json_path} does not exist.'))
+        return None
+    
+    # opens JSON, populates dict
+    with open(json_path, 'r') as infile:
+        playlist_dict = json.load(infile)
+
+    # checks to make sure dict keys are valid
+    dict_keys = set(playlist_dict.keys())
+    if dict_keys != REQUIRED_KEYS:
+        error_exit('Dictionary keys do not align.')
+    
+    return playlist_dict
+
+
+if __name__ == '__main__':
+    if not URL and not JSON:
+        error_exit('URL or JSON path not included.')
+    app.run(debug=True)
